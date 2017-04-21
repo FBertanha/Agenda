44ec361209b5f7f2b6050fb8c628d418553017dc
FBertanha <bertanhahp@hotmail.com>
2017-04-21 12:27:13 -0300

    Third Commit

--- app/src/main/java/br/com/alura/agenda/DetalhesProvaFragment.java
+++ app/src/main/java/br/com/alura/agenda/DetalhesProvaFragment.java
@@ -6,12 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 import java.util.List;
+
+import br.com.alura.agenda.modelo.Prova;
 
 public class DetalhesProvaFragment extends Fragment {
 
@@ -29,9 +32,25 @@ public class DetalhesProvaFragment extends Fragment {
         campoData = (TextView) view.findViewById(R.id.detalhes_prova_data);
         listaTopicos = (ListView) view.findViewById(R.id.detalhes_prova_topicos);
 
-        populaCampos(prova);
+        Bundle parametros = getArguments();
+
+        if (parametros != null) {
+            Prova prova = (Prova) parametros.getSerializable("prova");
+            populaCampos(prova);
+        }
+
+
 
         return view;
+    }
+
+    public void populaCampos(Prova prova) {
+        campoMateria.setText(prova.getMateria());
+        campoData.setText(prova.getData());
+
+        ArrayAdapter<String> adapterTopicos = new ArrayAdapter<>(getContext(), android.R.layout.simple_list_item_1, prova.getTopicos());
+
+        listaTopicos.setAdapter(adapterTopicos);
     }
 
 }
